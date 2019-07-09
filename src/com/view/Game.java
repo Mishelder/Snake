af13e3049ca127bc8249030eed1f384b2e639e79
Mishelder <48042524+Mishelder@users.noreply.github.com>
2019-07-09 22:29:58 +0300

    Game - created JFrame and JPanel

--- src/com/view/Game.java
+++ src/com/view/Game.java
@@ -1,7 +1,28 @@
 package com.view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Game extends JFrame {
+
+
+    private void initFrame(){
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private void initPanel(){
+        JPanel panel = new JPanel(){
+            protected void paintComponent(Graphics g){
+                super.paintComponent(g);
+            }
+        };
+        panel.setBackground(Color.BLACK);
+        panel.setPreferredSize(new Dimension(800,600));
+        add(panel);
+    }
+
 
 }
