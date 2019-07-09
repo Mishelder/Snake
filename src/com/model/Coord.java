b6057e5b36f188f05d1ef66a363b0de53494ca07
Mishelder <48042524+Mishelder@users.noreply.github.com>
2019-07-09 22:57:53 +0300

    Coord - created

---
+++ src/com/model/Coord.java
@@ -0,0 +1,20 @@
+package com.model;
+
+public class Coord {
+
+    private final int x;
+    private final int y;
+
+    Coord(final int x,final int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
