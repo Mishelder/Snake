25c630482c1a1dcd966192667b9446e94f2f354a
Mishelder <48042524+Mishelder@users.noreply.github.com>
2019-07-11 15:46:19 +0300

    Head - created

--- src/com/model/Head.java
+++ src/com/model/Head.java
@@ -1,57 +1,24 @@
 package com.model;
 
-import com.model.Snake;
+import java.awt.*;
 
 public class Head extends Snake {
 
-    private boolean left = false;
-    private boolean right = true;
-    private boolean up = false;
-    private boolean down = false;
-    private boolean inGame = true;
+    public static boolean left = false;
+    public static boolean right = true;
+    public static boolean up = false;
+    public static boolean down = false;
+    private Image head;
 
-    Head(final int x,final int y){
-        super(x,y);
+    public Head(final int x,final int y){
+        super(x,y,Figure.head);
     }
 
-
-    public boolean isLeft() {
-        return left;
+    public  Image getHead() {
+        return head;
     }
 
-    public void setLeft(boolean left) {
-        this.left = left;
-    }
-
-    public boolean isRight() {
-        return right;
-    }
-
-    public void setRight(boolean right) {
-        this.right = right;
-    }
-
-    public boolean isUp() {
-        return up;
-    }
-
-    public void setUp(boolean up) {
-        this.up = up;
-    }
-
-    public boolean isDown() {
-        return down;
-    }
-
-    public void setDown(boolean down) {
-        this.down = down;
-    }
-
-    public boolean isInGame() {
-        return inGame;
-    }
-
-    public void setInGame(boolean inGame) {
-        this.inGame = inGame;
+    public void setHead(Image head) {
+        this.head = head;
     }
 }
