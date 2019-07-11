b3f9bff30846fbb628d0997a8b13a292487d868b
Mishelder <48042524+Mishelder@users.noreply.github.com>
2019-07-11 15:45:57 +0300

    Body - created

--- src/com/model/Body.java
+++ src/com/model/Body.java
@@ -1,8 +1,21 @@
 package com.model;
 
+import java.awt.*;
+
 public class Body extends Snake {
 
+    private Image body;
+
     public Body(final int x,final int y) {
-        super(x, y);
+        super(x, y,Figure.body);
+    }
+
+    public Image getBody() {
+        return body;
+    }
+
+    public void setBody(Image body) {
+        this.body = body;
+
     }
 }
