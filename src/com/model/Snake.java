388a0e9cf2660b8c5a193cdab9025c555ab7b935
Mishelder <48042524+Mishelder@users.noreply.github.com>
2019-07-11 15:53:14 +0300

    Changed size image, created ArrayList for Figure, add method getImage

--- src/com/model/Snake.java
+++ src/com/model/Snake.java
@@ -2,12 +2,24 @@ package com.model;
 
 import java.util.ArrayList;
 
-class Snake {
+public class Snake {
 
-    private int image = 20;
-    private ArrayList<Coord> coordSnake = new ArrayList<>();
+    private int image = 25;
+    public static  ArrayList<Coord> coordSnake = new ArrayList<>();
+    public static  ArrayList<Figure> figureSnake = new ArrayList<>();
 
-    Snake(final int x,final int y){
+    public Snake(){}
+
+    Snake(final int x,final int y,final Figure figure){
         coordSnake.add(new Coord(x,y));
+        figureSnake.add(figure);
+    }
+
+    public ArrayList<Coord> getCoordSnake() {
+        return coordSnake;
+    }
+
+    public int getImage() {
+        return image;
     }
 }
