818ed6252e2da61f8e5a5508f80501adbcc38b19
Mishelder <48042524+Mishelder@users.noreply.github.com>
2019-07-11 15:45:43 +0300

    Apple - created

--- src/com/model/Apple.java
+++ src/com/model/Apple.java
@@ -2,21 +2,42 @@ package com.model;
 
 import java.awt.*;
 
-public class Apple implements Parametr {
+public class Apple implements Parameter {
 
-    private int X;
-    private int Y;
+    public static int X;
+    public static int Y;
     private Image apple;
-    private int image = 25;
 
-    Apple(){
+    public Apple(){
         randomApple();
-        X*=image;
-        Y*=image;
     }
 
     private void randomApple(){
         X=(int) (Math.random()*width);
         Y=(int) (Math.random()*height);
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public void setX(int x) {
+        X = x;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    public void setY(int y) {
+        Y = y;
+    }
+
+    public Image getApple() {
+        return apple;
+    }
+
+    public void setApple(Image apple) {
+        this.apple = apple;
     }
 }
