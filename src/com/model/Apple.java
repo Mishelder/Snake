79886445b99aaefe45be9bed06446279429540ee
Mishelder <48042524+Mishelder@users.noreply.github.com>
2019-07-16 20:40:22 +0300

    Deleted getters and variable Image because they don't use

--- src/com/model/Apple.java
+++ src/com/model/Apple.java
@@ -1,12 +1,9 @@
 package com.model;
 
-import java.awt.*;
-
 public class Apple implements Parameter {
 
     public static int X;
     public static int Y;
-    private Image apple;
 
     public Apple(){
         randomApple();
@@ -17,27 +14,5 @@ public class Apple implements Parameter {
         Y=(int) (Math.random()*height);
     }
 
-    public int getX() {
-        return X;
-    }
 
-    public void setX(int x) {
-        X = x;
-    }
-
-    public int getY() {
-        return Y;
-    }
-
-    public void setY(int y) {
-        Y = y;
-    }
-
-    public Image getApple() {
-        return apple;
-    }
-
-    public void setApple(Image apple) {
-        this.apple = apple;
-    }
 }
