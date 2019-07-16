ac25a7b6872f6c9a34b2bf3ec065412f3f8cb145
Mishelder <48042524+Mishelder@users.noreply.github.com>
2019-07-16 20:41:54 +0300

    Deleted getters and variable Image

--- src/com/model/Head.java
+++ src/com/model/Head.java
@@ -1,6 +1,5 @@
 package com.model;
 
-import java.awt.*;
 
 public class Head extends Snake {
 
@@ -8,17 +7,9 @@ public class Head extends Snake {
     public static boolean right = true;
     public static boolean up = false;
     public static boolean down = false;
-    private Image head;
 
     public Head(final int x,final int y){
         super(x,y,Figure.head);
     }
 
-    public  Image getHead() {
-        return head;
-    }
-
-    public void setHead(Image head) {
-        this.head = head;
-    }
 }
