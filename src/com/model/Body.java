54377fe4fb260286b89e46d4747c11921c33c6ee
Mishelder <48042524+Mishelder@users.noreply.github.com>
2019-07-16 20:41:27 +0300

    Deleted getter and variable Image because they don't use

--- src/com/model/Body.java
+++ src/com/model/Body.java
@@ -1,21 +1,11 @@
 package com.model;
 
-import java.awt.*;
-
 public class Body extends Snake {
 
-    private Image body;
 
     public Body(final int x,final int y) {
         super(x, y,Figure.body);
     }
 
-    public Image getBody() {
-        return body;
-    }
 
-    public void setBody(Image body) {
-        this.body = body;
-
-    }
 }
